faa78f8a619c9a7fa0dc8dc245b76b28797dece3
Jiarui Xu <77424292+Jiarui-Xu-Gatech@users.noreply.github.com>
2023-08-27 20:42:06 -0400

    Update README.m

--- README.m
+++ README.m
@@ -1,1 +1,22 @@
-This repo is for 7100
+This repo is for 7100 2023 Fall
+
+set dataset
+evaluation: Word Error Rate (WER)
+set original model
+set Noise Reprogramming 
+set CNN Reprogramming 
+set U-Net Reprogramming
+
+Timeline:
+First & Second Week: set original model
+Third Week: set dataset
+Forth Week: set Noise Reprogramming and train the model
+Fifth Week: train the model
+Sixth Week: Evaluate Noise reprogramming and set CNN Reprogramming
+Seventh Week: train the model and evaluate the CNN Reprogramming
+Eighth Week: set U-Net Model
+Ninth Week: train U-Net Model
+Tenth Week: Evaluate U-Net Model
+Eleventh Week: find other useful Reprogramming model if necessary
+Twelfth Week: train the model
+Thirteenth Week: prepare for the paper and discussion
